2bce6f0e19718eccd4a8f63a5a282e96d5d3d9f2
Mez <mezclone@gmail.com>
2017-12-04 18:29:53 -0800

    got input argument working

--- test.cpp
+++ test.cpp
@@ -23,19 +23,92 @@ short monsterMaker( int id, char gender ) {
 */
 #include "common.h"
 #include <stdio.h>
-    
-
-int main() {
-    int x = add( 5, 6 );
-    int x1 = add( 4, 2 );
-    int x2 = add( 7, 5 );
-    int x3 = add( 1,36 );
-    int x4 = add( 9, 7 );
-    
 
 
-    printf( "this will show only x=%d x1=%d x2=%d x3=%d x4=%d\n",
-            x, x1, x2, x3, x4  );
+int badAdd( int a, int b ) {
+    a = a + 1;
     
-    return x;
+    
+    return a + b;
+    }
+
+
+void addOne( int *a ) {
+    int t = *a;
+    t = t + 1;
+    
+    *a = t;
+    }
+
+
+int addAll( int count, int *a ) {
+    // write code to add all elements of array together
+    int sum = 0;
+    return sum;
+    }
+
+
+
+    
+
+int main( int numArgs, char *args[] ) {
+//int main( ) {
+    int a[5];
+
+    int x = 5;
+    int y = 10;
+    
+    x = 8; 
+
+
+    int *p = &x;
+    
+    p = &y;
+    
+    x = *p;
+    
+    const char *m = "This is a test";
+    //const char *n = "This is a test dsfaasd";
+    //const char *o = "This is a test asdfdsaf";
+    if(numArgs > 1  ) {
+        printf( "the first arg is %s\n", args[1] );
+        
+        }
+     
+    /*
+    const char *myStrings[3];
+    
+    myStrings[0] = m;
+    myStrings[1] = n;
+    myStrings[2] = o;
+    */
+
+    printf( "My string = %s\n", m );
+    
+
+
+    printf( "x = %d\n", x );
+    
+    addOne( &x );
+
+    printf( "x = %d\n", x );
+
+
+
+    a[0] = add( x, 6 );
+    a[1] = add( 4, 2 );
+    a[2] = add( 7, 5 );
+    a[3] = add( 1,36 );
+    a[4] = add( 9, 7 );
+    
+
+    int s = addAll( 5, a );
+
+    printf( "s=%d\n", s );
+    
+
+    printf( "this will show only a0=%d a1=%d a2=%d a3=%d a4=%d\n",
+            a[0], a[1], a[2], a[3], a[4]  );
+    
+    return 0;
     }
